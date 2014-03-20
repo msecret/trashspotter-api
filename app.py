504b371b41e9c6ce5bb92f964452400915a9667b
Marco Secreto <msegreto@miceover.com>
2014-03-19 20:47:27 -0700

    Setup heoku stuff

--- app.py
+++ app.py
@@ -2,14 +2,29 @@ import os
 
 import bottle
 from bottle import route
+import json
 import pymongo
 from pymongo import MongoClient
+
+from decorators import enable_cors
 
 client = MongoClient('localhost', 27017)
 db = client['dev-trashspotter']
 
-@route('/wakeup', method='GET')
-def get_wakeup():
+@route('/awake', method='GET')
+def get_awake():
   return 'f'
+
+@route('/garbage-can', method='POST')
+@enable_cors
+def post_garbage_can():
+  bottle.response.content_type = 'application/json'
+  return json.dumps({'success': True})
+
+@route('litter', method='POST')
+@enable_cors
+def post_litter():
+  return json.dumps({'success': True})
+
 
 bottle.run(server='gevent', port=os.environ.get('PORT', 5000))
