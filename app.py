96499ce7eb245946796911755f701f08a135dc19
Marco Secreto <msegreto@miceover.com>
2014-03-19 20:19:32 -0700

    Added stuff for mongo and reqs

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 
 import bottle
 from bottle import route
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['dev-trashspotter']
 
 @route('/wakeup', method='GET')
 def get_wakeup():
